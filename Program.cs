93e47f7e3ed48d50c22beeccef1f3d7fb2b681da
Md Shakil Ahamed <md.shakilahamedxox72@gmail.com>
2021-08-01 07:44:58 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Man jahangir = new Man();
+            jahangir.eat("rice");
+
+            jahangir.eat("rice","egg");
+        }
+
+
+    }
+
+
+class Man{
+    public void eat(String foodOne){
+   Console.WriteLine($"I am eating {foodOne}");
+    }
+    public void eat(String foodOne){
+   Console.WriteLine($"Eating {foodOne}");
+    }
+
+    public void eat(String foodOne,String foodTwo){
+   Console.WriteLine($"I am eating {foodOne} and {foodTwo}");
+    }
+}
+
+}
